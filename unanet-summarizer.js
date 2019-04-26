7ac35af27bdea2102a9f7492b9f8ba18dd946506
Jim Weir <jweir77@gmail.com>
2019-04-26 07:14:39 -0400

    Fix load error (#38)
    * Fixes load issue because summarizeUnanetTime was overwritten by null
    * Fixes load issue because summarizeUnanetTime was overwritten by null

--- unanet-summarizer.js
+++ unanet-summarizer.js
@@ -1,15 +1,18 @@
-window.summarizeUnanetTime = (function() {
-    
+window.summarizeUnanetTime = (function() {    
+    const REDIRECT_URL = 'https://excellalabs.github.io/unanet-summarizer/unanet-summarizer-release.js';
+
     var onload = function () {
         window.summarizeUnanetTimeForReal();
-    }
-    
-    const REDIRECT_URL = 'https://excellalabs.github.io/unanet-summarizer/unanet-summarizer-release.js';
-    
-    if (!window.summarizeUnanetTimeForReal) {
-        var s = document.createElement('script');
-        s.src = REDIRECT_URL;
-        s.onload = onload;
-        document.body.appendChild(s);
-    }
+    };
+      
+    return function() {
+        if (!window.summarizeUnanetTimeForReal) {
+            var s = document.createElement('script');
+            s.src = REDIRECT_URL;
+            s.onload = onload;
+            document.body.appendChild(s);
+        } else {
+            onload();
+        }
+    };
 })();
