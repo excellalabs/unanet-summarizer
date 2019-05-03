bee1b414901e7f153875d6f7fa85a7d29fde0e78
Sean Killeen <SeanKilleen@gmail.com>
2019-05-03 17:51:43 -0400

    Delete original files (#58)
    * delete now-unused files
    
    * Add explanation comments to JS file

--- unanet-summarizer.js
+++ unanet-summarizer.js
@@ -1,3 +1,13 @@
+
+/* 
+    NOTE: The reason this file uses a redirect is that it was originally served raw from GitHub pages.
+    We wanted to move to a build system, the contents of which would live elsewhere, but that would have broken things for existing users.
+    Rather than force people to update their bookmarks, we creatd a redirect to the files while they were on GitHub pages.
+    Once the build process was completed, we switched the URL to the new outputted location, and voila!
+
+    So, this file remains for backwards compatibility, rather than having bookmarklet users be confused as to why something stopped loading.
+*/
+
 window.summarizeUnanetTime = (function() {    
     const REDIRECT_URL = 'https://unanetsummarizer.blob.core.windows.net/unanet-summarizer/unanet-summarizer-release.js';
 
