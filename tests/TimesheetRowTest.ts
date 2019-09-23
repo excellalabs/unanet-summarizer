47f19a5b28dc0ffe219194b6f67f0566f4decb4b
Sean Killeen <SeanKilleen@gmail.com>
2019-09-23 19:39:59 -0400

    Cleanup: description organization

--- tests/TimesheetRowTest.ts
+++ tests/TimesheetRowTest.ts
@@ -1,27 +1,28 @@
 import {Summarizer} from "../src/classes/classes";
 
-describe('ctor', function(){
-  describe('entries', function(){
-
-    it('throws an error if the array is null', function(){
-      var shouldBlowUp = function(){
-        new Summarizer.TimesheetRow(Summarizer.ProjectType.Core, null);
-      };
-
-      expect(shouldBlowUp).toThrowError("Entries array must not be null or undefined.")
-    });
-
-    it('throws an error if the array is undefined', function(){
-      var shouldBlowUp = function(){
-        new Summarizer.TimesheetRow(Summarizer.ProjectType.Core, undefined);
-      };
-
-      expect(shouldBlowUp).toThrowError("Entries array must not be null or undefined.")
-    });
-  });
-});
 
 describe('TimesheetRow', function() {
+  describe('ctor', function(){
+    describe('entries', function(){
+  
+      it('throws an error if the array is null', function(){
+        var shouldBlowUp = function(){
+          new Summarizer.TimesheetRow(Summarizer.ProjectType.Core, null);
+        };
+  
+        expect(shouldBlowUp).toThrowError("Entries array must not be null or undefined.")
+      });
+  
+      it('throws an error if the array is undefined', function(){
+        var shouldBlowUp = function(){
+          new Summarizer.TimesheetRow(Summarizer.ProjectType.Core, undefined);
+        };
+  
+        expect(shouldBlowUp).toThrowError("Entries array must not be null or undefined.")
+      });
+    });
+  });
+  
   describe('Determining + Hours', function() {
     it('Treats BILL+ as Plus', function(){
       var row = new Summarizer.TimesheetRow(Summarizer.ProjectType.Bill, new Array<Summarizer.DateEntry>());
