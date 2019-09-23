79b0b659b21cf6abe81ae97133be425b51e4a87d
Sean Killeen <SeanKilleen@gmail.com>
2019-09-23 19:43:48 -0400

    some todos

---
+++ tests/TimesheetTest.ts
@@ -0,0 +1,8 @@
+import {Summarizer} from "../src/classes/classes";
+
+// TODO: Start Date
+// TODO: End Date
+// TODO: Latest timesheet entry date
+// TODO: Total + Hours
+// TODO: Total Non + Hours
+// TODO: Tracking
