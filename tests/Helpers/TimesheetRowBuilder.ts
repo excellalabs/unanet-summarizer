55eceff2c5a707f52ad9446acc26a110be9c78e9
Sean Killeen <SeanKilleen@gmail.com>
2019-09-27 23:49:12 -0400

    more linting

--- tests/Helpers/TimesheetRowBuilder.ts
+++ tests/Helpers/TimesheetRowBuilder.ts
@@ -3,20 +3,20 @@ import { ProjectType } from "../../src/classes/ProjectType";
 import { TimesheetRow } from "../../src/classes/TimesheetRow";
 
 export class TimesheetRowBuilder {
-  projectType: ProjectType;
-  entries = new Array<DateEntry>();
+  private projectType: ProjectType;
+  private entries = new Array<DateEntry>();
 
-  build = () => {
-    var objToReturn = new TimesheetRow(this.projectType, this.entries);
+  public build = () => {
+    const objToReturn = new TimesheetRow(this.projectType, this.entries);
     return objToReturn;
   };
 
-  withEntry = (entry: DateEntry) => {
+  public withEntry = (entry: DateEntry) => {
     this.entries.push(entry);
     return this;
   };
 
-  withProjectType = (type: ProjectType) => {
+  public withProjectType = (type: ProjectType) => {
     this.projectType = type;
     return this;
   };
