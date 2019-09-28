59dfa0fc6d0e5b95a3df908bbc7f9def9fa53ebe
Sean Killeen <SeanKilleen@gmail.com>
2019-09-27 23:46:48 -0400

    more linting

--- tests/Helpers/TimesheetBuilder.ts
+++ tests/Helpers/TimesheetBuilder.ts
@@ -4,36 +4,36 @@ import { Timesheet } from "../../src/classes/Timesheet";
 import { TimesheetRow } from "../../src/classes/TimesheetRow";
 
 export class TimesheetBuilder {
-  startDate: string = "2019-09-01";
-  endDate: string = "2019-09-15";
-  todayDate: string = "2019-09-08";
-  rows = new Array<TimesheetRow>();
+  private startDate: string = "2019-09-01";
+  private endDate: string = "2019-09-15";
+  private todayDate: string = "2019-09-08";
+  private rows = new Array<TimesheetRow>();
 
   constructor() {
     this.rows.push(new TimesheetRow(ProjectType.Bench, new Array<DateEntry>()));
   }
 
-  withStartDate = (theDate: string) => {
+  public withStartDate = (theDate: string) => {
     this.startDate = theDate;
     return this;
   };
 
-  withEndDate = (theDate: string) => {
+  public withEndDate = (theDate: string) => {
     this.endDate = theDate;
     return this;
   };
 
-  withTodayDate = (theDate: string) => {
+  public withTodayDate = (theDate: string) => {
     this.todayDate = theDate;
     return this;
   };
 
-  withRows = (theRows: Array<TimesheetRow>) => {
+  public withRows = (theRows: TimesheetRow[]) => {
     this.rows = theRows;
     return this;
   };
 
-  build = () => {
+  public build = () => {
     return new Timesheet(
       this.rows,
       this.startDate,
