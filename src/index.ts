beeff76f8b5b7d16f0cad22ceb07c617f163d055
Sean Killeen <SeanKilleen@gmail.com>
2019-09-28 22:27:17 -0400

    global & more linting

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,64 @@
-interface Window { 
-    summarizeUnanetTimeForReal: any; 
+import { Summarizer } from "./classes/Summarizer";
+
+declare global {
+  // tslint:disable-next-line:interface-name
+  interface Window {
+    summarizeUnanetTimeForReal: any;
+  }
 }
 
-const template = require('./summary-template.hbs');
-const css = require('./summarizer-style.css').default;
-const summarize = require('./unanet-summarizer');
+const template = require("./summary-template.hbs");
+const css = require("./summarizer-style.css").default;
 
-window.summarizeUnanetTimeForReal = (function() {
-  const CONTAINER_ID = 'unanet-summarizer';
-  const STYLESHEET_ID = 'unanet-summarizer-style';
-  const CSS_CLASS = 'unanet-summary';
-  const TIMESHEET_FORM_ID = 'time';
+window.summarizeUnanetTimeForReal = (() => {
+  const timesheetTable = document.querySelector("table.timesheet");
+  const summarizer = new Summarizer(window.document.location.href, window.document.title, timesheetTable);
 
-    var createContainer = function() {
-        var container = document.createElement('div');
-      container.id = CONTAINER_ID;
-      container.className = CSS_CLASS;
-      return document.body.insertBefore(container, document.body.firstChild);
+  const CONTAINER_ID = "unanet-summarizer";
+  const STYLESHEET_ID = "unanet-summarizer-style";
+  const CSS_CLASS = "unanet-summary";
+  const TIMESHEET_FORM_ID = "time";
+
+  var createContainer = () => {
+    var container = document.createElement("div");
+    container.id = CONTAINER_ID;
+    container.className = CSS_CLASS;
+    return document.body.insertBefore(container, document.body.firstChild);
   };
 
-  var getContainer = function() {
-      return document.getElementById(CONTAINER_ID) || createContainer();
+  var getContainer = () => {
+    return document.getElementById(CONTAINER_ID) || createContainer();
   };
 
-  var getTimesheetForm = function() {
-      return document.getElementById(TIMESHEET_FORM_ID);
+  var getTimesheetForm = () => {
+    return document.getElementById(TIMESHEET_FORM_ID);
   };
 
-  var createStylesheet = function() {
-      var style = document.createElement('style');
-      style.id = STYLESHEET_ID;
-      style.appendChild(document.createTextNode(css));
-      return document.head.appendChild(style);
+  var createStylesheet = () => {
+    var style = document.createElement("style");
+    style.id = STYLESHEET_ID;
+    style.appendChild(document.createTextNode(css));
+    return document.head.appendChild(style);
   };
 
-  var getStylesheet = function() {
-      return document.getElementById(STYLESHEET_ID) || createStylesheet();
+  var getStylesheet = () => {
+    return document.getElementById(STYLESHEET_ID) || createStylesheet();
   };
 
-  var onInputChanged = function(event: { target: any; }) {
-      if (event.target instanceof HTMLInputElement) {
-          getContainer().innerHTML = template(summarize());
-      }
+  var onInputChanged = function(event: { target: any }) {
+    if (event.target instanceof HTMLInputElement) {
+      getContainer().innerHTML = template(summarizer.timesheet);
+    }
   };
 
   return function() {
-      var stylesheet = getStylesheet();
-      var timesheetForm = getTimesheetForm();
+    var stylesheet = getStylesheet();
+    var timesheetForm = getTimesheetForm();
 
-      if (timesheetForm) {
-          timesheetForm.addEventListener('change', onInputChanged);
-      }
+    if (timesheetForm) {
+      timesheetForm.addEventListener("change", onInputChanged);
+    }
 
-      getContainer().innerHTML = template(summarize());
+    getContainer().innerHTML = template(summarizer.timesheet);
   };
 })();
