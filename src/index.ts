7b55775cad6f827d8ff98983e50d46725c56bdb8
Sean Killeen <SeanKilleen@gmail.com>
2019-10-17 17:26:20 -0400

    add to template object

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ window.summarizeUnanetTimeForReal = (() => {
     const theSummary = {
       grandTotalHours: summarizer.timesheet.totalPlusHours() + summarizer.timesheet.totalNonPlusHours(),
       hoursByProjectType: summarizer.timesheet.hoursByProjectType(),
+      hoursPerWorkday: (summarizer.timesheet.totalPlusHours() + summarizer.timesheet.totalNonPlusHours()) / summarizer.timesheet.weekdaysInTimesheet(),
       negativeTracking: summarizer.timesheet.plusHoursTracking() < 0,
       plusHoursInPayPeriod: summarizer.timesheet.expectedPlusHours(),
       plusHoursTracking: summarizer.timesheet.plusHoursTracking(),
