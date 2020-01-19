2c50de3a229fb52e2de577fa3cbbf6c370b76bda
Sean Killeen <SeanKilleen@gmail.com>
2020-01-19 10:26:38 -0500

    function files

--- analytics/AnalyticsHttpTrigger.cs
+++ analytics/AnalyticsHttpTrigger.cs
@@ -10,24 +10,25 @@ using Newtonsoft.Json;
 
 namespace Excella.Function
 {
-    public static class AnalyticsHttpTrigger
+  public static class AnalyticsHttpTrigger
+  {
+    [FunctionName("AnalyticsHttpTrigger")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [Table("AnalyticsEntries")]ICollector<AnalyticsEntry> tableBinding,
+        ILogger log)
     {
-        [FunctionName("AnalyticsHttpTrigger")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+      log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+      string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+      string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+      dynamic data = JsonConvert.DeserializeObject(requestBody);
+      name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
+      return name != null
+          ? (ActionResult)new OkObjectResult($"Hello, {name}")
+          : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
     }
+  }
 }
