7a669fe6732972ca51bcb363c11ef0c011cce46c
Sean Killeen <SeanKilleen@gmail.com>
2020-01-19 11:06:11 -0500

    Analytics project via Azure functions (#193)
    * create function project
    
    * function files
    
    * Add functions storage package
    
    * Update the security level to anonymous
    
    * Function content
    
    * Update table name
    
    * update table name

---
+++ analytics/AnalyticsEntry.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Excella.Function
+{
+  public class AnalyticsEntry
+  {
+    public DateTime Timestamp { get; set; }
+    public string UserName { get; set; }
+    public string TimesheetUser { get; set; }
+
+  }
+}
---
+++ analytics/AnalyticsHttpTrigger.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Excella.Function
+{
+  public static class AnalyticsHttpTrigger
+  {
+    [FunctionName("AnalyticsHttpTrigger")]
+    public static async Task<IActionResult> Run(
+
+        // NOTE: We are choosing to use AnalyticsEntry here which will attempt to modelbind for us. We could also use HttpRequest and get the full request.
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] AnalyticsEntry incomingModelBoundObject,
+        [Table("analyticsentries")]ICollector<AnalyticsEntry> tableBinding,
+        ILogger log)
+    {
+      log.LogInformation("Incoming analytics entry: {AnalyticsEntry}", incomingModelBoundObject);
+      log.LogInformation("C# HTTP trigger function processed a request.");
+
+      if (string.IsNullOrWhiteSpace(incomingModelBoundObject.UserName))
+      {
+        log.LogWarning("User's name was not provided");
+        return new BadRequestResult(); ;
+      }
+
+      if (string.IsNullOrWhiteSpace(incomingModelBoundObject.TimesheetUser))
+      {
+        log.LogWarning("Timesheet user was not provided");
+        return new BadRequestResult();
+      }
+
+      tableBinding.Add(incomingModelBoundObject);
+      return new OkResult();
+    }
+  }
+}
