079883ae9c6193201a4fa225f1d3afab0163093f
Sean Killeen <SeanKilleen@gmail.com>
2020-01-19 15:38:32 -0500

    use string for RowStamp too

--- analytics/AnalyticsEntry.cs
+++ analytics/AnalyticsEntry.cs
@@ -5,15 +5,16 @@ namespace Excella.Function
 {
   public class AnalyticsEntry
   {
-    public DateTime RowKey { get; }
+    public string RowKey { get; }
     public string PartitionKey { get; }
     public string UserName { get; }
     public string TimesheetUser { get; }
 
     public AnalyticsEntry(AnalyticsEntryRequest request)
     {
-      this.RowKey = request.Timestamp;
-      this.PartitionKey = request.Timestamp.ToString("o");
+      var timestampString = request.Timestamp.ToString("o");
+      this.RowKey = timestampString;
+      this.PartitionKey = timestampString;
       this.UserName = request.UserName;
       this.TimesheetUser = request.TimesheetUser;
 
