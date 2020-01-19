550944f6eff553c18fc1041f70670febd6d4eab3
Sean Killeen <SeanKilleen@gmail.com>
2020-01-19 13:27:44 -0500

    convert ParitionKey to string

--- analytics/AnalyticsEntry.cs
+++ analytics/AnalyticsEntry.cs
@@ -6,14 +6,14 @@ namespace Excella.Function
   public class AnalyticsEntry
   {
     public DateTime RowKey { get; }
-    public DateTime PartitionKey { get; }
+    public string PartitionKey { get; }
     public string UserName { get; }
     public string TimesheetUser { get; }
 
     public AnalyticsEntry(AnalyticsEntryRequest request)
     {
       this.RowKey = request.Timestamp;
-      this.PartitionKey = request.Timestamp;
+      this.PartitionKey = request.Timestamp.ToString("o");
       this.UserName = request.UserName;
       this.TimesheetUser = request.TimesheetUser;
 
