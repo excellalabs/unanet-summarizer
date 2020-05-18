3db36f6cf620f801c983b38e2e31f438dea5874f
Sean Killeen <SeanKilleen@gmail.com>
2020-05-18 09:48:50 -0400

    use new CopyPlugin syntax

--- bookmarklet/webpack.config.development.js
+++ bookmarklet/webpack.config.development.js
@@ -1,16 +1,16 @@
-const webpackConfig = require('./webpack.config');
-const CopyPlugin = require('copy-webpack-plugin');
+const webpackConfig = require("./webpack.config");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = Object.assign(webpackConfig, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      inline: false
-    },
-    plugins: [
-      new CopyPlugin([
-        { from: './src/bookmarklet.html', to: 'index.html' }
-      ])
-    ]
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    inline: false,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./src/bookmarklet.html", to: "index.html" }],
+    }),
+  ],
 });
