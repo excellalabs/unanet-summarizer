20b4a15c52dfb1695efab188350aeecba23c1b4d
agriffi10 <agriffi10@gmail.com>
2022-10-10 18:55:16 -0400

    reduced size of moment js

--- bookmarklet/webpack.config.js
+++ bookmarklet/webpack.config.js
@@ -1,25 +1,55 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'unanet-summarizer-release.js'
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "unanet-summarizer-release.js",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "async",
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    module: {
-      rules: [
-        { test: /\.css$/, use: 'raw-loader' },
-        { test: /\.hbs$/, use: 'handlebars-loader' },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        }   
-      ]
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ]
-    },
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: "raw-loader" },
+      { test: /\.hbs$/, use: "handlebars-loader" },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
+  ],
 };
