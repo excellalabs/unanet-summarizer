7198af7c23ec49764a3bf1db80662d51836fdc73
agriffi10 <agriffi10@gmail.com>
2022-10-10 20:22:19 -0400

    Undoing unneccessary webpack config changes

--- bookmarklet/webpack.config.js
+++ bookmarklet/webpack.config.js
@@ -9,29 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "unanet-summarizer-release.js",
   },
-  optimization: {
-    splitChunks: {
-      chunks: "async",
-      minSize: 20000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
   module: {
     rules: [
       { test: /\.css$/, use: "raw-loader" },
