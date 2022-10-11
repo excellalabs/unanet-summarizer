46ab9a85bd52533cb49d9571283cb2555ecdc362
Andrew G <30501888+agriffi10@users.noreply.github.com>
2022-10-10 20:38:38 -0400

    Reduce summarizer bundle (#680)
    * reduced size of moment js
    
    * Undoing unneccessary webpack config changes

--- bookmarklet/webpack.config.development.js
+++ bookmarklet/webpack.config.development.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const webpackConfig = require("./webpack.config");
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const WebpackBundleAnalyzer = require("webpack-bundle-analyzer");
 
 module.exports = Object.assign(webpackConfig, {
   mode: "development",
@@ -14,6 +16,11 @@ module.exports = Object.assign(webpackConfig, {
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "./src/bookmarklet.html", to: "index.html" }],
+    }),
+    new WebpackBundleAnalyzer.BundleAnalyzerPlugin(),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
     }),
   ],
 });
--- bookmarklet/webpack.config.js
+++ bookmarklet/webpack.config.js
@@ -1,25 +1,32 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'unanet-summarizer-release.js'
-    },
-    module: {
-      rules: [
-        { test: /\.css$/, use: 'raw-loader' },
-        { test: /\.hbs$/, use: 'handlebars-loader' },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        }   
-      ]
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ]
-    },
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "unanet-summarizer-release.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: "raw-loader" },
+      { test: /\.hbs$/, use: "handlebars-loader" },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
+  ],
 };
